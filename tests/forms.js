cc417b88c26628c4c68033d40d1bbc600b19aac8
Sam Richard <sam@snug.ug>
2016-05-18 07:40:34 -0400

    :unamused: Clean up old test assertions

--- tests/forms.js
+++ tests/forms.js
@@ -22,6 +22,6 @@ test('Retrieve data from a form', t => {
   t.is(data['service-desciption'], 'Foo service lorem ipsum description', 'should return desc from response');
   t.is(data['service-url'], 'foo@bar.com', 'should return url from response');
   t.is(data.id, '07b45c32-001d-4c46-a785-0025b04f7f37', 'should return id from response');
-  t.ok(data.created !== formResponse.body.created, '`created` datetime should be re-created by function');
-  t.ok(data.created > formResponse.body.created, 'new `created` datetime variable will be more recent');
+  t.true(data.created !== formResponse.body.created, '`created` datetime should be re-created by function');
+  t.true(data.created > formResponse.body.created, 'new `created` datetime variable will be more recent');
 });
