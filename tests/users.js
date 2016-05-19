ada99dd07708e3a220eb53adbb668bf7bee4a573
Scott Nath <snath@us.ibm.com>
2016-05-19 15:49:32 -0400

    :white_check_mark: users ct creation tests

---
+++ tests/users.js
@@ -0,0 +1,43 @@
+import test from 'ava';
+import users from '../lib/users';
+
+
+const usersContentObj = {
+  'name': 'Users',
+  'description': 'Test users description',
+  'id': 'users',
+  'attributes': [
+    {
+      'type': 'email',
+      'id': 'email',
+      'name': 'Email',
+      'description': 'Your email is your username',
+    },
+  ],
+};
+
+test('Exports exist', t => {
+  t.is(typeof users, 'object', 'Users exports a function');
+
+  t.is(typeof users.userContentType, 'function', 'Submodule `userContentType` exists and is a function');
+  t.is(typeof users.userRoutes, 'function', 'Submodule `userRoutes` exists and is a function');
+  t.is(typeof users.checkUserTable, 'function', 'Submodule `checkUserTable` exists and is a function');
+});
+
+test('Users merged with correct param', t => {
+  return users.userContentType(usersContentObj)
+    .then(result => {
+      t.is(result[0].name, 'Users', 'Get users content type name');
+      t.is(result[0].description, 'Test users description', 'Get users content type desc');
+      t.is(result[0].id, 'users', 'Get users content type id');
+    });
+});
+
+test('Users merged with correct param', t => {
+  return users.userContentType()
+    .then(result => {
+      t.is(result[0].name, 'Users', 'Get users content type name');
+      t.is(result[0].description, 'A users content model.', 'Get users content type desc');
+      t.is(result[0].id, 'users', 'Get users content type id');
+    });
+});
