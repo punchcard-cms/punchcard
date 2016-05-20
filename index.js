f6cc8a2f73bef1eb0b880f00f53221344ed71beb
Sam Richard <sam@snug.ug>
2016-05-20 11:24:34 -0400

    :new: Add Initialization

--- index.js
+++ index.js
@@ -6,109 +6,103 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const logger = require('morgan');
-const nunjucks = require('nunjucks');
 const path = require('path');
 const config = require('config');
 const contentTypes = require('punchcard-content-types');
 
 const indexRoutes = require('./lib/routes/index');
 const contentTypesRoutes = require('./lib/routes/content-types');
-const session = require('./lib/auth/sessions');
 
-let app = express();
+const init = require('./lib/init');
 
-app = session(app);
+let application = express();
 
-// Nunjucks templating setup
-const views = [
-  path.join(process.cwd(), 'views'),
-  path.join(process.cwd(), 'templates'),
-  path.join(__dirname, 'views'),
-  path.join(__dirname, 'templates'),
-];
+// Initialize the Database
+application = () => {
+  return init(application).then(initialized => {
+    const app = initialized;
 
-nunjucks.configure(views, {
-  'autoescape': true,
-  'express': app,
-});
-app.set('view engine', 'html');
+    // TODO: where does `dev` come from; should control with config
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(express.static(path.join(process.cwd(), 'public')));
 
+    /*
+      @name sitewide persistent variables
+      @description create variables which will work in any route
+     */
+    app.locals.siteName = 'Punchcard CMS';
+    app.locals.contentTypesConfig = config.contentTypes;
+    contentTypes().then(types => {
+      // make content type object available to entire express app
+      app.locals.contentTypesNames = types;
 
-// TODO: where does `dev` come from; should control with config
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(process.cwd(), 'public')));
-
-/*
-  @name sitewide persistent variables
-  @description create variables which will work in any route
- */
-app.locals.siteName = 'Punchcard CMS';
-app.locals.contentTypesConfig = config.contentTypes;
-contentTypes().then(types => {
-  // make content type object available to entire express app
-  app.locals.contentTypesNames = types;
-
-  return;
-}).catch(err => {
-  console.error(err);
-});
-
-/*
-  @name home page route
-  @description create route for the site landing page
- */
-app.use('/', indexRoutes);
-
-/*
-  @name content types route
-  @description create routes for all content type configurations in ./content-types
- */
-app.use('/content', contentTypesRoutes);
-
-/*
-  @name 404
-  @description catch 404 and forward to error handler
- */
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
+      return;
+    }).catch(err => {
+      console.error(err);
     });
-  });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {},
+    /*
+      @name home page route
+      @description create route for the site landing page
+     */
+    app.use('/', indexRoutes);
+
+    /*
+      @name content types route
+      @description create routes for all content type configurations in ./content-types
+     */
+    app.use('/content', contentTypesRoutes);
+
+    /*
+      @name 404
+      @description catch 404 and forward to error handler
+     */
+    app.use((req, res, next) => {
+      const err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
+
+    // error handlers
+
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+      app.use((err, req, res) => {
+        res.status(err.status || 500);
+        res.render('error', {
+          message: err.message,
+          error: err,
+        });
+      });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use((err, req, res) => {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: {},
+      });
+    });
+
+    return app;
   });
-});
+};
 
 
 /*
   @description run the server if and only if this file is being run directly
  */
 if (!module.parent) {
-  app.listen(config.env.port, () => {
-    console.log(`Server starting on ${config.env.url}`);
+  application().then(app => {
+    app.listen(config.env.port, () => {
+      console.log(`Server starting on ${config.env.url}`);
+    });
   });
 }
 
-module.exports = app;
+module.exports = application;
