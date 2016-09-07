e17d7ef6b2e7967e1c4c326767a1f9164cc545f8
Sam Richard <sam@snug.ug>
2016-09-07 15:37:02 -0400

    :white_check_mark: Add Schedule tests

---
+++ tests/schedule.js
@@ -0,0 +1,138 @@
+import test from 'ava';
+import moment from 'moment';
+import Promise from 'bluebird';
+import sutils from '../lib/schedule/utils';
+import putils from '../lib/utils';
+import utils from './fixtures/_utils';
+import db from '../lib/database';
+
+const items = utils.generate();
+const length = items.content.length;
+const count = Math.floor(Math.random() * length);
+
+test('Setup', t => {
+  const item = count;
+  const revision = items.content[item];
+
+  return sutils.setup(revision).then(() => {
+    return db('schedule').select('*').where({
+      id: revision.id,
+      revision: revision.revision,
+    });
+  }).then(result => {
+    t.is(result.length, 1, 'One item added');
+    t.deepEqual(result[0].attributes, revision.value);
+  }).catch(e => {
+    t.fail(e.message);
+  });
+});
+
+test('Push', t => {
+  const item = count + 1 > length - 1 ? 1 : count + 1;
+  const revision = items.content[item];
+
+  revision.sunset = null;
+
+  return sutils.setup(revision).then(() => {
+    return sutils.push(revision);
+  }).then(() => {
+    return db('live').select('*').where({
+      id: revision.id,
+      revision: revision.revision,
+    });
+  }).then(result => {
+    t.is(result.length, 1, 'One item added');
+    t.deepEqual(result[0].attributes, revision.value);
+  }).catch(e => {
+    console.log(e);
+    t.fail(e.message);
+  });
+});
+
+
+test('Pull', t => {
+  const item = count + 2 > length - 1 ? 2 : count + 2;
+  const revision = items.content[item];
+
+  revision.sunset = putils.time.iso(moment().format('YYYY-MM-DD'), moment().subtract(1, 'minute').format('hh:mm'), 'America/New_York');
+
+  return db('live').insert({
+    'id': revision.id,
+    'revision': revision.revision,
+    'language': revision.language,
+    'sunrise': revision.sunrise,
+    'sunset': revision.sunset,
+    'attributes': revision.value,
+    'type': revision.type,
+    'key': revision.identifier,
+    'type-slug': revision['type-slug'],
+    'key-slug': revision.slug,
+  }).then(() => {
+    return sutils.pull(revision);
+  }).then(() => {
+    return db('live').select('*').where({
+      id: revision.id,
+      revision: revision.revision,
+    });
+  }).then(result => {
+    t.is(result.length, 0, 'Item deleted');
+  }).catch(e => {
+    t.fail(e.message);
+  });
+});
+
+test('Sunrise', t => {
+  const item = count + 3 > length - 1 ? 3 : count + 3;
+  const revision = items.content[item];
+
+  revision.sunset = null;
+
+  return sutils.setup(revision).then(() => {
+    return sutils.sunrise(revision);
+  }).then(() => {
+    return Promise.delay(200);
+  }).then(() => {
+    return db('live').select('*').where({
+      id: revision.id,
+    });
+  }).then(result => {
+    t.is(result.length, 1, 'One item added');
+    t.deepEqual(result[0].attributes, revision.value);
+  }).catch(e => {
+    console.log(e);
+    t.fail(e.message);
+  });
+});
+
+test('Sunset', t => {
+  const item = count + 4 > length - 1 ? 4 : count + 4;
+  const revision = items.content[item];
+
+  revision.sunset = putils.time.iso(moment().format('YYYY-MM-DD'), moment().format('hh:mm'), 'America/New_York');
+
+  return db('live').insert({
+    'id': revision.id,
+    'revision': revision.revision,
+    'language': revision.language,
+    'sunrise': revision.sunrise,
+    'sunset': revision.sunset,
+    'attributes': revision.value,
+    'type': revision.type,
+    'key': revision.identifier,
+    'type-slug': revision['type-slug'],
+    'key-slug': revision.slug,
+  }).then(() => {
+    return sutils.sunset(revision);
+  }).then(() => {
+    return Promise.delay(200);
+  }).then(() => {
+    return db('live').select('*').where({
+      id: revision.id,
+      revision: revision.revision,
+    });
+  }).then(result => {
+    t.is(result.length, 0, 'Item deleted');
+  }).catch(e => {
+    t.fail(e.message);
+  });
+});
