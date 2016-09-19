14c50bae67af1becfe32bb9e984aa8d00516054c
Scott Nath <snath@us.ibm.com>
2016-09-19 10:26:17 -0400

    :white_check_mark: basic routing tests set up

--- tests/applications.js
+++ tests/applications.js
@@ -3,9 +3,53 @@ import events from 'events';
 import httpMocks from 'node-mocks-http';
 import _ from 'lodash';
 
-import routes from '../lib/applications/routes';
+import applications from '../lib/applications';
 
 const EventEmitter = events.EventEmitter;
+
+
+test('Applications functions', t => {
+  t.is(typeof applications.routes, 'object', '`routes` exists and is an object');
+  t.is(typeof applications.routes.all, 'function', '`all` exists and is a function');
+  t.is(typeof applications.routes.add, 'function', '`new` exists and is a function');
+  t.is(typeof applications.routes.one, 'function', '`one` exists and is a function');
+  t.is(typeof applications.routes.save, 'function', '`save` exists and is a function');
+  t.is(typeof applications.model, 'function', '`model` exists and is a function');
+  t.is(typeof applications.model.structure, 'object', '`structure` exists and is an object');
+});
+
+//////////////////////////////
+// Applications object structure
+//////////////////////////////
+test('Applications structure object', t => {
+  const structure = applications.model.structure;
+
+  t.is(typeof structure, 'object', 'Structure is an object');
+  t.is(structure.name, 'Applications', 'Structure has name');
+  t.is(structure.description, 'Contains webhook applications', 'Structure has description');
+  t.is(structure.id, 'applications', 'Structure has id');
+  t.true(Array.isArray(structure.attributes), 'attributes is an array');
+});
+
+//////////////////////////////
+// Applications - model
+//////////////////////////////
+test('Applications model', t => {
+  return applications.model().then(model => {
+    t.true(model[0].hasOwnProperty('name'), 'Should have a workflow attribute');
+    t.is(model[0].name, 'Applications', 'Structure has name');
+  });
+});
+
+test('Workflow model from config', t => {
+  const structure = _.cloneDeep(applications.model.structure);
+  structure.name = 'Other';
+
+  return applications.model(structure).then(model => {
+    t.true(model[0].hasOwnProperty('name'), 'Should have a workflow attribute');
+    t.is(model[0].name, 'Other', 'Structure has name');
+  });
+});
 
 //////////////////////////////
 // Routes - Applications landing
@@ -17,7 +61,7 @@ test.cb('All applications route', t => {
   });
 
   const response = httpMocks.createResponse({ eventEmitter: EventEmitter });
-  routes.all(request, response);
+  applications.routes.all(request, response);
 
   response.on('end', () => {
     const data = response._getRenderData();
@@ -39,7 +83,7 @@ test.cb('New application route', t => {
   });
 
   const response = httpMocks.createResponse({ eventEmitter: EventEmitter });
-  routes.add(request, response);
+  applications.routes.add(request, response);
 
   response.on('end', () => {
     const data = response._getRenderData();
@@ -61,7 +105,7 @@ test.cb('Single application route', t => {
   });
 
   const response = httpMocks.createResponse({ eventEmitter: EventEmitter });
-  routes.one(request, response);
+  applications.routes.one(request, response);
 
   response.on('end', () => {
     const data = response._getRenderData();
@@ -83,11 +127,9 @@ test.cb('Save application route', t => {
   });
 
   const response = httpMocks.createResponse({ eventEmitter: EventEmitter });
-  routes.save(request, response);
+  applications.routes.save(request, response);
 
   response.on('end', () => {
-    const data = response._getRenderData();
-
     t.is(response.statusCode, 302, 'Should be a 302 response');
     t.is(response._getRedirectUrl(), '/applications');
     t.end();
