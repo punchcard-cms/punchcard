9367fae1892a7fb63a20924250e0e4f4550a0681
Sam Richard <sam@snug.ug>
2016-11-08 08:23:24 -0500

    :white_check_mark: Add Storage tests

---
+++ tests/fixtures/files/index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const ipsum = require('lorem-ipsum');
+const uuid = require('uuid');
+const path = require('path');
+const fs = require('fs');
+
+const files = [
+  path.join(process.cwd(), '', 'storage.js'),
+  path.join(process.cwd(), '..', 'index.js'),
+  path.join(process.cwd(), '..', 'Gulpfile.js'),
+  path.join(process.cwd(), '../src/images', 'bee.svg'),
+  path.join(process.cwd(), '../src/images', 'punchcard-image.svg'),
+  path.join(process.cwd(), '../src/images', 'punchcard-404.svg'),
+];
+
+const fixtures = () => {
+  const items = [];
+  let counter = files.length - 1;
+  do {
+    counter = Math.round(Math.random() * (files.length - 1));
+
+    let type = path.extname(files[counter]) === '.js' ? 'document/javascript' : 'image/svg';
+
+    items.push({
+      fieldName: `file-upload--file-${counter}`,
+      originalFilename: path.basename(files[counter]),
+      path: files[counter],
+      headers: {
+        'content-disposition': `form-data; name="file-upload--file-${counter}"; filename="${path.basename(files[counter])}"`,
+        'content-type': type,
+      },
+      size: fs.statSync(files[counter]).size,
+      name: path.basename(files[counter]),
+      type,
+    });
+
+    files.splice(counter, 1);
+  } while (files.length > 0 && Math.random() > .5);
+
+
+  return items;
+}
+
+
+module.exports = fixtures;
