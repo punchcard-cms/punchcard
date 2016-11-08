7603d6a5150c7e5789ebab3140a742b8263e4c53
Sam Richard <sam@snug.ug>
2016-11-08 11:30:02 -0500

    :white_check_mark: Add get tests

--- tests/storage.js
+++ tests/storage.js
@@ -1,17 +1,19 @@
 import test from 'ava';
 import del from 'del';
 import buffertools from 'buffertools';
+import _ from 'lodash';
 import fs from 'fs';
+import path from 'path';
 import storage from '../lib/storage';
 import fixtures from './fixtures/files';
-
-const input = fixtures();
 
 test.after.always(() => {
   return del(['public']);
 });
 
 test('Storage - Default', t => {
+  const input = fixtures.raw();
+
   return storage.put(input).then(files => {
     t.is(Object.keys(files).length, input.length, 'Same number of files came out as went in');
 
@@ -26,6 +28,7 @@ test('Storage - Default', t => {
       t.true(result.hasOwnProperty('_rel'), 'Has a relative path');
       t.is(result.type, expected.type, 'Have the same type');
       t.is(result.stat.size, expected.size, 'Same size files');
+      t.is(result.rel, path.basename(result.path), 'Relative URL is the same as basename of path');
 
       const output = fs.readFileSync(result.path);
       const original = fs.readFileSync(expected.path);
@@ -36,4 +39,32 @@ test('Storage - Default', t => {
     console.error(e.stack);
     t.fail();
   });
+});
+
+test('Storage - Get', t => {
+  const input = fixtures.saved();
+  const results = storage.get(_.cloneDeep(input));
+
+  t.true(input.length === results.length, 'Equal number of inputs and results');
+
+  input.forEach(expected => {
+    const result = results.find(r => {
+      return r.name === expected.name;
+    });
+
+    if (result === false) {
+      t.fail('No result for input item');
+    }
+
+    t.true(result.hasOwnProperty('name'), 'Result has a name');
+    t.true(result.hasOwnProperty('path'), 'Result has a path')
+    t.true(result.hasOwnProperty('type'), 'Result has a type');
+
+    t.is(result.name, expected.name, 'Result name is same as expected name');
+    t.is(result.type, expected.type, 'Result name is same as expected name');
+
+    t.true(result.path.indexOf(expected.path) >= 0, 'Result path contains expected path');
+  });
+
+  t.pass();
 });
