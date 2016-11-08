232f9c5812d2b505843c820beff228d4e60ab301
Scott Nath <snath@us.ibm.com>
2016-11-08 12:46:58 -0500

    :bug: fix null bug

--- tests/apis.js
+++ tests/apis.js
@@ -275,7 +275,7 @@ test.serial('Utils: Format Results - with follow', t => {
   return formatted.then(result => {
     result.forEach(itm => {
       utils.formatted(t, itm, query);
-      if (typeof itm !== 'null') {
+      if (itm !== null) {
         t.true(itm.hasOwnProperty('attributes'), 'Contains attributes');
 
         utils.depths(t, itm.attributes, query);
