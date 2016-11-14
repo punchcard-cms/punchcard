514d62bdc06b4e938dd0ef25b308da17efd908e3
Scott Nath <snath@us.ibm.com>
2016-11-14 14:25:51 -0500

    :white_check_mark: fix tests for less code

--- tests/users-middleware.js
+++ tests/users-middleware.js
@@ -3,7 +3,6 @@ import uuid from 'uuid';
 import cloneDeep from 'lodash/cloneDeep';
 
 import middleware from '../lib/users/middleware';
-import utils from '../lib/users/middleware/utils';
 
 /**
  * Next
@@ -22,97 +21,19 @@ const req = {
   },
 };
 
-//////////////////////////////
-// Utils - id check
-//////////////////////////////
-test('Check user id-bad blank', t => {
-  const rq = cloneDeep(req);
-
-  return utils.id(rq).catch(err => {
-    t.is(typeof err, 'string', 'Blank id fails');
-    t.is(err, 'User ID must be in UUID format', 'Should fail with a message');
-  });
-});
-
-test('Check user id-bad empty string', t => {
-  const rq = cloneDeep(req);
-  rq.params.id = '';
-
-  return utils.id(rq).catch(err => {
-    t.is(typeof err, 'string', 'Non-uuid id fails');
-    t.is(err, 'User ID must be in UUID format', 'Should fail with a message');
-  });
-});
-
-test('Check user id-bad object', t => {
-  const rq = cloneDeep(req);
-  rq.params.id = {};
-
-  return utils.id(rq).catch(err => {
-    t.is(typeof err, 'string', 'Non-uuid id fails');
-    t.is(err, 'User ID must be in UUID format', 'Should fail with a message');
-  });
-});
-
-test('Check user id-bad string', t => {
-  const rq = cloneDeep(req);
-  rq.params.id = 'foo';
-
-  return utils.id(rq).catch(err => {
-    t.is(typeof err, 'string', 'Non-uuid id fails');
-    t.is(err, 'User ID must be in UUID format', 'Should fail with a message');
-  });
-});
-
-test('Check user id-bad just a number', t => {
-  const rq = cloneDeep(req);
-  rq.params.id = 123;
-
-  return utils.id(rq).catch(err => {
-    t.is(typeof err, 'string', 'Non-uuid id fails');
-    t.is(err, 'User ID must be in UUID format', 'Should fail with a message');
-  });
-});
-
-test('Check user id - no id param', t => {
-  const rq = cloneDeep(req);
-  delete rq.params.id;
-
-  return utils.id(rq).then(result => {
-    t.true(result, 'No id returns true');
-  });
-});
-
-test('Check user id - good ADD action', t => {
-  const rq = cloneDeep(req);
-  rq.params.id = 'add';
-
-  return utils.id(rq).then(result => {
-    t.true(result, 'Action add returns true');
-  });
-});
-
-test('Check user id - good SAVE action', t => {
-  const rq = cloneDeep(req);
-  rq.params.id = 'save';
-
-  return utils.id(rq).then(result => {
-    t.true(result, 'Action add returns true');
-  });
-});
-
-test('Check user id - IS uuid', t => {
-  const rq = cloneDeep(req);
-  rq.params.id = uuid.v4();
-
-  return utils.id(rq).then(result => {
-    t.true(result, 'Good id returns true');
-  });
-});
 
 //////////////////////////////
 // Utils - URL check
 //////////////////////////////
+test('URL checks id is there at all', t => {
+  const rq = cloneDeep(req);
+
+  return middleware(rq, {}, next).then(() => {
+    t.pass();
+  }).catch(() => {
+    t.fail();
+  });
+});
 test('URL checks id is uuid', t => {
   const rq = cloneDeep(req);
   rq.params.id = 'foo';
