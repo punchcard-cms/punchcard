e2f59007ae5df09cc7175d1d554b3ab7ea017671
Scott Nath <snath@us.ibm.com>
2016-12-19 11:43:48 -0500

    :fire: raw function in shared-tests

--- tests/fixtures/files/index.js
+++ tests/fixtures/files/index.js
@@ -14,42 +14,6 @@ const all = [
 ];
 
 /*
- * Raw File Fixture
- *
- * @returns {array} - An array file objects as one receives from a multi-upload form
- */
-const raw = () => {
-  const files = _.cloneDeep(all);
-  const items = [];
-  let counter = 0;
-  let random;
-  do {
-    random = Math.round(Math.random() * (files.length - 1));
-
-    const type = path.extname(files[random]) === '.js' ? 'document/javascript' : 'image/svg';
-
-    items.push({
-      fieldName: `file-upload--file-${counter}`,
-      originalFilename: path.basename(files[random]),
-      path: files[random],
-      headers: {
-        'content-disposition': `form-data; name="file-upload--file-${counter}"; filename="${path.basename(files[random])}"`,
-        'content-type': type,
-      },
-      size: fs.statSync(files[random]).size,
-      name: path.basename(files[random]),
-      type,
-    });
-
-    files.splice(random, 1);
-    counter++;
-  } while (files.length > 0 && Math.random() > 0.5);
-
-
-  return items;
-};
-
-/*
  * Saved File Fixtures
  *
  * @returns {array} - Array of objects describing files as it's stored in the database. Object contains the original file's file name (name), the relative path to the file plugin (path), and the file's MIME type (type)
@@ -64,8 +28,8 @@ const saved = () => {
     const type = path.extname(files[counter]) === '.js' ? 'document/javascript' : 'image/svg';
 
     items.push({
-      name: path.basename(files[counter]),
-      path: `${uuid.v4()}${path.extname(files[counter])}`,
+      original: path.basename(files[counter]),
+      relative: `${uuid.v4()}${path.extname(files[counter])}`,
       type,
     });
 
@@ -78,6 +42,5 @@ const saved = () => {
 
 
 module.exports = {
-  raw,
   saved,
 };
